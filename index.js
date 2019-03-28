4bc2e27c779a8b4455a417f865bc2e86b7b65dad
lydiathornton <lydia.d.thornton@gmail.com>
2019-03-28 10:47:54 -0700

    trouble shoot server errors

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
-const greeting = process.env.GREETING;
 
 server.listen(port, () => {
-  console.log(
-    `\n*** ${greeting} Server Running on http://localhost:${port} ***\n`
-  );
+  console.log(`\n***  Server Running on http://localhost:${port} ***\n`);
 });
