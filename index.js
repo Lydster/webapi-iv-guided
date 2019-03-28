0fd8727990d86a355d35304a21ad7c24403cde98
lydiathornton <lydia.d.thornton@gmail.com>
2019-03-28 10:56:50 -0700

    Working locally

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-server.listen(port, () => {
-  console.log(`\n***  Server Running on http://localhost:${port} ***\n`);
+server.listen(PORT, () => {
+  console.log(`\n***  Server Running on http://localhost:${PORT} ***\n`);
 });
